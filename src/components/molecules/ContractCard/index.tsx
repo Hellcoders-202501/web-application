a290a79946f28c6a649005b7228839b85ad83ac1
Eduardo Sebastián Lévano Cavero <79949221+lecav30@users.noreply.github.com>
2025-05-13 00:43:25 -0500

    feat: request contract card

--- src/components/molecules/ContractCard/index.tsx
+++ src/components/molecules/ContractCard/index.tsx
@@ -2,7 +2,7 @@ import Button from "@components/atoms/Button";
 import { FC } from "react";
 import { FaRegUserCircle } from "react-icons/fa";
 
-export type ContractVariant = "offer" | "pending" | "history";
+export type ContractVariant = "offer" | "pending" | "history" | "request";
 
 interface Props {
 	// contract: Trip;
@@ -37,67 +37,9 @@ const ContractCard: FC<Props> = ({ variant = "history", userType }) => {
 
 	if (variant === "offer" || variant === "pending")
 		return (
-      <div className="card px-10 py-8 min-h-[370px] max-w-[405px]">
-        <p className="text-xl font-semibold">Resume</p>
-        <div className="mt-4">
-          <p>
-            From: <b>Comas</b>
-          </p>
-          <p>
-            To: <b>Los Olivos</b>
-          </p>
-          <p>
-            Date: <b>12/11/23</b>
-          </p>
-          <p>
-            Capacity: <b>20</b>
-          </p>
-        </div>
-        <div
-          className="flex justify-between font-semibold text-lg border-t mt-4
-                    pt-2"
-        >
-          <p>Client data</p>
-          <p>Amount</p>
-        </div>
-        <div className="flex justify-between items-end gap-10">
-          <div>
-            <div className="flex gap-2 items-center">
-              <FaRegUserCircle size={32} />
-              <div className="text-sm">
-                <p>Oscar Canellas</p>
-                <p>983288372</p>
-              </div>
-            </div>
-          </div>
-          <p className="text-2xl font-bold">S/. 1560</p>
-        </div>
-        <div className="mt-4 flex gap-6">
-          <Button variant="accept" className="flex-1/2">
-            {defineText()}
-          </Button>
-          <Button variant="denied" className="flex-1/2">
-            {variant === "pending" ? "Cancel" : "Decline"}
-          </Button>
-        </div>
-      </div>
-    );
-
-	if (variant === "history")
-		return (
-			<div className="card flex gap-5 px-10 py-8 min-h-[200px] max-w-[405px]">
-				<div>
-					<FaRegUserCircle size={100} />
-					<div className="text-center mt-2">
-						<p className="font-semibold">Driver</p>
-						<p>Oscar Canellas</p>
-					</div>
-				</div>
-				<div>
-					<p className="font-semibold">Details:</p>
-					<p className="mt-4">
-						Date: <b>12/11/23</b>
-					</p>
+			<div className="card px-10 py-8 min-h-[370px] max-w-[405px]">
+				<p className="text-xl font-semibold">Resume</p>
+				<div className="mt-4">
 					<p>
 						From: <b>Comas</b>
 					</p>
@@ -105,9 +47,77 @@ const ContractCard: FC<Props> = ({ variant = "history", userType }) => {
 						To: <b>Los Olivos</b>
 					</p>
 					<p>
-						Amount: <b>S/. 450</b>
+						Date: <b>12/11/23</b>
+					</p>
+					<p>
+						Capacity: <b>20</b>
 					</p>
 				</div>
+				<div
+					className="flex justify-between font-semibold text-lg border-t mt-4
+                    pt-2"
+				>
+					<p>Client data</p>
+					<p>Amount</p>
+				</div>
+				<div className="flex justify-between items-end gap-10">
+					<div>
+						<div className="flex gap-2 items-center">
+							<FaRegUserCircle size={32} />
+							<div className="text-sm">
+								<p>Oscar Canellas</p>
+								<p>983288372</p>
+							</div>
+						</div>
+					</div>
+					<p className="text-2xl font-bold">S/. 1560</p>
+				</div>
+				<div className="mt-4 flex gap-6">
+					<Button variant="accept" className="flex-1/2">
+						{defineText()}
+					</Button>
+					<Button variant="denied" className="flex-1/2">
+						{variant === "pending" ? "Cancel" : "Decline"}
+					</Button>
+				</div>
+			</div>
+		);
+
+	if (variant === "history" || variant === "request")
+		return (
+			<div>
+				<div className="card flex gap-5 px-10 py-8 min-h-[200px] max-w-[405px]">
+					<div>
+						<FaRegUserCircle size={100} />
+						<div className="text-center mt-2">
+							<p className="font-semibold">Driver</p>
+							<p>Oscar Canellas</p>
+						</div>
+					</div>
+					<div>
+						<p className="font-semibold">Details:</p>
+						<p className="mt-4">
+							Date: <b>12/11/23</b>
+						</p>
+						<p>
+							From: <b>Comas</b>
+						</p>
+						<p>
+							To: <b>Los Olivos</b>
+						</p>
+						<p>
+							Amount: <b>S/. 450</b>
+						</p>
+					</div>
+				</div>
+				{variant === "request" && (
+					<div className="flex justify-center mt-4">
+						<Button className="w-1/2">Edit</Button>
+						<Button className="w-1/2 ml-4" variant="denied">
+							Delete
+						</Button>
+					</div>
+				)}
 			</div>
 		);
 };
--- src/views/App/Requests/index.tsx
+++ src/views/App/Requests/index.tsx
@@ -1,17 +1,64 @@
 "use client";
+import ContractCard from "@components/molecules/ContractCard";
 import useRequestService from "./hooks/useRequestService";
 import RequestForm from "@components/organisms/RequestForm";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import { Fragment } from "react";
 
 const RequestView = () => {
 	const { requestState } = useRequestService();
 
 	return (
-		<div
-			className="flex flex-col items-center max-w-5xl mx-auto w-10/12 lg:w-auto 
-			my-10 lg:my-20"
-		>
-			<p className="self-start mb-5 text-4xl font-semibold">Request service</p>
-			<RequestForm requestState={requestState} />
+		<div className="flex flex-col justify-center max-w-4xl mx-auto w-10/12 lg:w-auto my-10">
+			<TabGroup className="w-full">
+				<TabList className="flex w-full border-b">
+					<Tab as={Fragment}>
+						{({ hover, selected }) => (
+							<div className="flex justify-center outline-0 w-1/2">
+								<button
+									className={`${(hover || selected) && "border-b-4 border-accept"}`}
+									type="button"
+								>
+									Make a Request
+								</button>
+							</div>
+						)}
+					</Tab>
+					<Tab as={Fragment}>
+						{({ hover, selected }) => (
+							<div className="flex justify-center outline-0 w-1/2">
+								<button
+									className={`${(hover || selected) && "border-b-4 border-accept"}`}
+									type="button"
+								>
+									List of Requests
+								</button>
+							</div>
+						)}
+					</Tab>
+				</TabList>
+				<TabPanels className="mt-3">
+					<TabPanel
+						className="flex flex-col md:flex-row flex-wrap justify-between gap-y-10
+						mt-10 max-w-5xl mx-auto w-10/12 lg:w-auto "
+					>
+						<p className="self-start mb-5 text-4xl font-semibold">
+							Request service
+						</p>
+						<RequestForm requestState={requestState} />
+					</TabPanel>
+					<TabPanel
+						className="flex flex-col md:flex-row flex-wrap justify-between gap-y-10
+						mt-10 max-w-5xl mx-auto w-10/12 lg:w-auto "
+					>
+						{Array(5)
+							.fill(null)
+							.map((_, index) => (
+								<ContractCard key={index} variant="request" />
+							))}
+					</TabPanel>
+				</TabPanels>
+			</TabGroup>
 		</div>
 	);
 };
